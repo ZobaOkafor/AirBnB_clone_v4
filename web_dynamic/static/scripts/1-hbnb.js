4c4c68fdc3b41478cca8a5016390f0d579d38edd
Queendarlin Nnamani <queendarlinnnamani@gmail.com>
2024-05-04 21:48:33 +0100

    Task2

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,27 +1,11 @@
-$(document).ready(function() {
-    // Initialize an empty array to store Amenity IDs
-    let amenityIDs = [];
-
-    // Listen for changes on each input checkbox tag
-    $('input[type="checkbox"]').change(function() {
-        const amenityID = $(this).data('id');
-        const amenityName = $(this).data('name');
-
-        // Check if the checkbox is checked
-        if ($(this).is(':checked')) {
-            // Add Amenity ID to the array if checked
-            amenityIDs.push(amenityID);
-        } else {
-            // Remove Amenity ID from the array if unchecked
-            const index = amenityIDs.indexOf(amenityID);
-            if (index !== -1) {
-                amenityIDs.splice(index, 1);
-            }
-        }
-
-        // Update the h4 tag inside the div Amenities with the list of Amenities checked
-        const amenitiesList = amenityIDs.map(id => $('#' + id).data('name')).join(', ');
-
-        $('.amenities h4').text(amenitiesList);
-    });
+document.ready(function () {
+	const amenities = {};
+	$("li input[type=checkbox]").change(function () {
+		if (this.checked) {
+			amenities[this.dataset.name] = this.dataset.id;
+		} else {
+			delete amenities[this.dataset.name];
+		}
+		$(".amenities h4").text(Object.keys(amenities).sort().join(", "));
+	});
 });
